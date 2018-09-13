bc39a745c64431b81d23bf98986f9a4b553ab00f
isaacupy <al1709071@upy.edu.mx>
2018-09-13 16:35:10 -0500

    First Commit

---
+++ main.js
@@ -0,0 +1,33 @@
+function btnempty(who){
+  he = document.getElementByID(who);
+  he.removeAttribute('style');
+  yesclick(who);
+};
+
+function btnfull(who){
+  he = document.getElementByID(who);
+  he.setAttribute('style', "red");
+  noclick(who);
+};
+
+function yesclick(who){
+  he = document.getElementByID();
+};
+
+function noclick(who){
+
+};
+
+function tolink(who){
+
+};
+function rememberlink(who){
+  he = document.getElementByID(who);
+  ami = he.getAttribute()
+  whoelse = ["nora", ]
+  for (var i = 0; i < whoelse.length; i++) {
+    if (ami != whoelse[i]){
+
+    };
+  }
+};
